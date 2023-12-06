c6dfb610acdc54fc22b3aaf2e3caa7ef4882bf01
AlexisCh9132 <alexis.chandelle@gmail.com>
2023-12-06 15:11:16 +0100

    Initial Commit

---
+++ ProjectMuseumApp/ProjectMuseumAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectMuseumAppApp.swift
+//  ProjectMuseumApp
+//
+//  Created by Alexis Chandelle on 12/6/23.
+//
+
+import SwiftUI
+
+@main
+struct ProjectMuseumAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
