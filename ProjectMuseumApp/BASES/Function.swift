9edc05539b6828fc9df8b1eeb2384c5b0e618ec1
AlexisCh9132 <alexis.chandelle@gmail.com>
2023-12-07 15:04:42 +0100

    Mise à part des fonctions + documentation

--- ProjectMuseumApp/BASES/Function.swift
+++ ProjectMuseumApp/BASES/Function.swift
@@ -6,3 +6,57 @@
 //
 
 import Foundation
+
+/**
+ Cette fonction renvoie la date et l'heure d'ouverture d'un musée.
+ 
+ - Returns: Un objet `Date` représentant l'heure d'ouverture du musée.
+ */
+func openHour() -> Date {
+    var dateComponentOpen = DateComponents()
+    dateComponentOpen.hour = 9
+    dateComponentOpen.minute = 30
+    
+    let open = Calendar.current.date(from: dateComponentOpen)!
+    
+    return open
+}
+
+/**
+ Cette fonction renvoie la date et l'heure de fermeture d'un musée.
+ 
+ - Returns: Un objet `Date` représentant l'heure de fermeture du musée.
+ */
+func closeHour() -> Date {
+    var dateComponentClose = DateComponents()
+    dateComponentClose.hour = 17
+    dateComponentClose.minute = 30
+    
+    let close = Calendar.current.date(from: dateComponentClose)!
+    
+    return close
+}
+
+/**
+ Cette fonction renvoie la date actuelle au format "année-mois-jour" en chaîne de caractères.
+ 
+ - Returns: Une chaîne de caractères représentant la date au format "yyyy-MM-dd".
+ */
+func returnDate() -> String {
+    let formater = DateFormatter()
+    formater.dateFormat = "yyyy-MM-dd"
+    
+    return formater.string(from: Date())
+}
+
+/**
+ Cette fonction renvoie l'heure actuelle au format "heure:minute" en chaîne de caractères.
+ 
+ - Returns: Une chaîne de caractères représentant l'heure au format "HH:mm".
+ */
+func returnHour() -> String {
+    let formater = DateFormatter()
+    formater.dateFormat = "HH:mm"
+    
+    return formater.string(from: Date())
+}
