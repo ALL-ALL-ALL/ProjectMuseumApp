19bca9065b237337dd203646f9022bbf502e2ccb
AlexisCh9132 <alexis.chandelle@gmail.com>
2023-12-08 10:18:27 +0100

    Changement de nom à TabView pour la vu principal

--- ProjectMuseumApp/ProjectMuseumAppApp.swift
+++ ProjectMuseumApp/ProjectMuseumAppApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ProjectMuseumAppApp: App {
     var body: some Scene {
         WindowGroup {
-            PrincipalView()
+            TabView()
         }
     }
 }
---
+++ ProjectMuseumApp/TabView.swift
@@ -0,0 +1,33 @@
+//
+//  TabView.swift
+//  ProjectMuseumApp
+//
+//  Created by Alexis Chandelle on 12/6/23.
+//
+
+import SwiftUI
+
+struct TabView: View {
+    var body: some View {
+        VStack{
+            TabView(selection: .constant(1)) {
+                MuseumList().tabItem {Image(systemName: "building.columns.fill")
+                    Text("Musée")
+                }.tag(1)
+                Text("Guide").tabItem {Image(systemName: "book.closed.fill")
+                    Text("Guide")
+                }.tag(2)
+                Text("Billeterie").tabItem { Image(systemName: "ticket.fill")
+                    Text("Billeterie")
+                }.tag(3)
+                Text("Compte").tabItem {Image(systemName: "person.fill")
+                    Text("Compte")
+                }.tag(4)
+            }
+        }
+    }
+}
+
+#Preview {
+    TabView()
+}
