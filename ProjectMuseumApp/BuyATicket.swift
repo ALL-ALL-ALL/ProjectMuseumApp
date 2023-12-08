b96b885a411a6a00045c5798b2b26ff3af00f900
AlexisCh9132 <alexis.chandelle@gmail.com>
2023-12-08 10:23:51 +0100

    Création de la view pour acheter un ticket

---
+++ ProjectMuseumApp/BuyATicket.swift
@@ -0,0 +1,21 @@
+//
+//  BuyATicket.swift
+//  ProjectMuseumApp
+//
+//  Created by Alexis Chandelle on 12/8/23.
+//
+
+import SwiftUI
+
+struct BuyATicket: View {
+    var museumSelect:Int
+    var body: some View {
+        VStack{
+            Text("Oui")
+        }
+    }
+}
+
+#Preview {
+    BuyATicket(museumSelect: 0)
+}
